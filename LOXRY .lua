746c116617c9a277b6553e8bdd0752c776f8231c
LOXRYCHEATS <90551687+LOXRYCHEATS@users.noreply.github.com>
2021-09-12 13:32:57 +0300

    Update LOXRY .lua

--- LOXRY .lua
+++ LOXRY .lua
@@ -1,14 +1,1 @@
-gg.alert("KEYİ GİRİNİZ")
-PW = gg.prompt({'🔒 ᴀɴᴀʜᴛᴀʀ ꜱɪꜰʀᴇꜱɪɴɪ ɢɪʀɪɴ : '},{[1]=''},{[1]='text'})
-if not PW then return
-end 
-if PW[1] == "" then gg.alert("ʟᴏXʀʏ ᴠɪᴘ ꜱᴄ ᴀɴᴀʜᴛᴀʀɪ") os.exit() end
-if PW[1] =="190_6667_LoxRy_TR_XLAY" then print("𝐀𝐍𝐀𝐇𝐓𝐀𝐑  190_6667_LoxRy_TR_XLAY ")
-print("𝐀𝐍𝐀𝐇𝐓𝐀𝐑  190_6667_LoxRy_TR_XLAY")
- gg.toast('✨《 𝐒̧İ𝐅𝐑𝐄 𝐃𝐎𝐆̆𝐑𝐔 𝐇İ𝐋𝐄𝐘𝐄 𝐔𝐋𝐀𝐒̧𝐀𝐁İ𝐋İ𝐑𝐒İ𝐍 》✨')
-else 
- gg.alert(" YANLIŞ😡") print("𝚂̧İ𝙵𝚁𝙴 190_6667_LoxRy_TR_XLAY") return end 
-gg.sleep(900)
-gg.toast("%10")
-gg.sleep(900)
-gg.toast("%20")
+gg.alert("BAKIMDA")
